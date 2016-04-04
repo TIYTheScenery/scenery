0a14616f0a01bdd1d762c3c8672a0a84fc58f7ed
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-04-04 14:27:06 -0400

    Add strong params for performances on performance controller

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -1,5 +1,9 @@
 class PerformanceController < ApplicationController
   def create
-    
+
+  end
+  private
+  def performance_params
+    params.require(:performance).permit(:id, :name, :description, :owner_id, :company_id, :trailer_link, :ticket_link)
   end
 end
