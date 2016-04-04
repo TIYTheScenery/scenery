3c1b037e5909a3bc045f967de15586f6bf9192aa
Steven Cooper <stevengcooper76@gmail.com>
2016-04-04 17:44:13 -0400

    Commiting to switch to master.

--- app/controllers/show_times_controller.rb
+++ app/controllers/show_times_controller.rb
@@ -8,6 +8,13 @@ class ShowTimesController < ApplicationController
   def show
   end
 
+  def index
+  end
+
   def destroy
+  end
+
+  private def show_time_params
+    params.require(:show_time).permit(:event_id, :event_type, :begin_time, :end_time, :venue_id, :address, :city, :state, :zip_code, :date)
   end
 end
