549f42d25c9add7bfcfa4edcd71a0e47a72d396b
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-04-04 19:13:27 -0400

    more dirty dirty fixes for the CORS error

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,15 +10,15 @@ class ApplicationController < ActionController::Base
   def cors_set_access_control_headers
       headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Allow-Headers'] = '*'
       headers['Access-Control-Max-Age'] = "1728000"
   end
 
   def cors_preflight_check
     if request.method == "OPTIONS"
-      headers['Access-Control-Allow-Origin'] = 'http://localhost'
+      headers['Access-Control-Allow-Origin'] = '*'
       headers['Access-Control-Allow-Methods'] => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
-      headers['Access-Control-Allow-Headers'] = %w{Origin Accept Content-Type X-Requested-With auth_token X-CSRF-Token}.join(',')
+      headers['Access-Control-Allow-Headers'] = '*'
       headers['Access-Control-Max-Age'] = '1728000'
       render :text => '', :content_type => 'text/plain'
     end
