9c3ce1efe38f7e5e3d3f67b3b24372db5366c71f
Hugh Robert Labusohr The 2nd <hughlabusohr@mac.com>
2016-04-04 14:54:44 -0400

    Create a create action in Performance controller

--- app/controllers/performance_controller.rb
+++ app/controllers/performance_controller.rb
@@ -1,8 +1,14 @@
 class PerformanceController < ApplicationController
   def create
+    @performance = Performance.new(performance_params)
 
+    if @performance.save
+      redirect_to performances_path, notice: "Performance was successfully created."
+    else
+      render :new
+    end
   end
-  
+
   private
   def performance_params
     params.require(:performance).permit(:id, :name, :description, :owner_id, :company_id, :trailer_link, :ticket_link)
