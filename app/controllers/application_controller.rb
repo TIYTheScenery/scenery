4fad73fa4db672afab2e5b5f40596f6555525c38
Steven Cooper <stevengcooper76@gmail.com>
2016-04-05 08:53:52 -0400

    Removing dirty fixes from last commit.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,27 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery
-  before_filter :cors_preflight_check
-  after_filter :cors_set_access_control_headers
+  
 
   # For all responses in this controller, return the CORS access control headers.
 
-  def cors_set_access_control_headers
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
-      headers['Access-Control-Allow-Headers'] = '*'
-      headers['Access-Control-Max-Age'] = "1728000"
-  end
 
-  def cors_preflight_check
-    if request.method == "OPTIONS"
-      headers['Access-Control-Allow-Origin'] = '*'
-      headers['Access-Control-Allow-Methods'] => 'POST, PUT, PATCH, DELETE, GET, OPTIONS',
-      headers['Access-Control-Allow-Headers'] = '*'
-      headers['Access-Control-Max-Age'] = '1728000'
-      render :text => '', :content_type => 'text/plain'
-    end
-  end
 
   def authenticate_user(token)
     user = User.find_by(login_token: token)
