09b6b722ff28d0f681135cf988dacd2d61c6bd20
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-04-06 17:59:48 -0400

    another quick search fix

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,10 @@ class SearchController < ApplicationController
     location = params[:location].split(',').map(&:strip)
     location << 0 if location.length == 1
     @performances = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
-        where("genre_performances.genre_id = ?", params[:genre_id]).
-        joins("JOIN show_times ON performances.id = show_times.event_id").
-        where("((show_times.event_type = 'Performance' AND (show_times.address LIKE ? OR (show_times.city LIKE ? AND show_times.state LIKE ? ) OR show_times.zip_code LIKE ? )) OR (performances.name LIKE ? OR performances.description LIKE ? ))",
-            "%#{location[0]}%", "%#{location[0]}%", "%#{location[1]}%", "%#{location[0]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%" )
+        where("genre_performances.genre_id = ? AND (performances.name LIKE ? OR performances.description LIKE ? )", params[:genre_id], "%#{params[:search_term]}%", "%#{params[:search_term]}%" )
+        # joins("JOIN show_times ON performances.id = show_times.event_id").
+        # where("((show_times.event_type = 'Performance' AND (show_times.address LIKE ? OR (show_times.city LIKE ? AND show_times.state LIKE ? ) OR show_times.zip_code LIKE ? )) OR (performances.name LIKE ? OR performances.description LIKE ? ))",
+        #     "%#{location[0]}%", "%#{location[0]}%", "%#{location[1]}%", "%#{location[0]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%" )
   end
 
   private def search_params
