923064011708cd51d335739c5bb90d18c7cc680e
Steven Cooper <stevengcooper76@gmail.com>
2016-04-06 18:40:06 -0400

    Added dependent destroy to performance.rb for nested showtimes.

--- app/models/performance.rb
+++ app/models/performance.rb
@@ -4,7 +4,7 @@ class Performance < ActiveRecord::Base
 
   validates :name, presence: true
   validates :description, presence: true
-  has_many :show_times, as: :event
+  has_many :show_times, as: :event, dependent: :destroy
 
   accepts_nested_attributes_for :show_times,
       reject_if: :all_blank,
