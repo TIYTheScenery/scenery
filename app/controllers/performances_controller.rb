8526d9121e542a8bd29baf52ad222b3e41e955ae
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-04-06 17:51:27 -0400

    temporary fix for search and performance pass through is kinda wierd in this one

--- app/controllers/performances_controller.rb
+++ app/controllers/performances_controller.rb
@@ -13,7 +13,8 @@ class PerformancesController < ApplicationController
   private
   def performance_params
     params.require(:performance).permit(:id, :name, :description, :owner_id, :company_id, :trailer_link, :ticket_link,
-    show_times_attributes: [:id, :event_id, :begin_time, :end_time, :venue_id, :address, :city, :state, :zip_code, :date, :_destroy],)
+    show_times_attributes: [:id, :event_id, :begin_time, :end_time, :venue_id, :address, :city, :state, :zip_code, :date, :_destroy],
+    genre_performances_attributes: [:id, :performance_id, :genre_id, genres_attributes: [:id]])
   end
   def user_params
     params.require(:user_info).permit(:login_token)
--- app/models/performance.rb
+++ app/models/performance.rb
@@ -1,11 +1,12 @@
 class Performance < ActiveRecord::Base
   has_many :genre_performances
   has_many :genres, through: :genre_performances
-  
+
   validates :name, presence: true
   validates :description, presence: true
   has_many :show_times, as: :event
 
+  accepts_nested_attributes_for :genre_performances
   accepts_nested_attributes_for :show_times,
       reject_if: :all_blank,
       allow_destroy: true
