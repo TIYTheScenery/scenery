0f31274ceb6568022ccb1c6419b9a7cb6497ba55
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-04-06 18:26:29 -0400

    search should work with showtimes

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,10 +3,16 @@ class SearchController < ApplicationController
     location = params[:location].split(',').map(&:strip)
     location << 0 if location.length == 1
     @performances = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
-        where("genre_performances.genre_id = ? AND ", params[:genre_id]).
+        where("genre_performances.genre_id = ? ", params[:genre_id]).
         joins("JOIN show_times ON performances.id = show_times.event_id").
-        where("(show_times.event_type = 'Performance' AND (LOWER(show_times.address) LIKE LOWER(?) OR (LOWER(show_times.city) LIKE LOWER(?) AND LOWER(show_times.state) LIKE LOWER(?) ) OR show_times.zip_code LIKE ? )) OR (LOWER(performances.name) LIKE LOWER(?) OR LOWER(performances.description) LIKE LOWER(?) )",
+        where("show_times.event_type = 'Performance' AND " +
+              "(LOWER(show_times.address) LIKE LOWER(?) OR " +
+                "(LOWER(show_times.city) LIKE LOWER(?) AND LOWER(show_times.state) LIKE LOWER(?) ) " +
+              "OR show_times.zip_code LIKE ? ) OR " +
+              "(LOWER(performances.name) LIKE LOWER(?) OR LOWER(performances.description) LIKE LOWER(?) )",
             "%#{location[0]}%", "%#{location[0]}%", "%#{location[1]}%", "%#{location[0]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%" )
+
+    byebug
   end
 
   private def search_params
