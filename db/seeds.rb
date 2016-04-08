c853c1f361a9f43892590f8e9a7890b7e55f98a4
Steven Cooper <stevengcooper76@gmail.com>
2016-04-08 15:03:40 -0400

    Debugged tests with changed UserTitle rather than UsersTitle.

--- db/seeds.rb
+++ db/seeds.rb
@@ -45,27 +45,3 @@ if Rails.env.development?
     temp.show_times << ShowTime.create!( begin_time: Faker::Time.forward(23, :evening), address: Faker::Address.street_address, city: Faker::Address.city, state: "NC", zip_code: Faker::Address.zip_code, date: Faker::Date.forward(23))
   end
 end
-# 
-# Performance.create!(owner_id: "", company_id: "", name: "Master Harold…and the boys by Athol Fugard", description: "Presented by Mortall Coile Theatre Company. Directed by Jesse Gephart. Featuring Gil Faison, George Hill, and Ben Pluska", trailer_link: "", ticket_link: "")
-#
-# ShowTime.create!( begin_time: (Time.now), address: "209 Oberlin Rd", city: Raleigh, state: "NC", zip_code: 27605, date: Date.new)
-#
-# Performance.create!(name: "Annie Jr The Musical", description: "The Musical.  Presented by CheerUp Productions")
-#
-# ShowTime.create!( begin_time: (Time.now), address: 209 Oberlin Rd, city: Raleigh, state: "NC", zip_code: 27605, date: Date.new)
-#
-# Performance.create!(name: "Anne of Green Gables", description: "Presented by the students of Sonorous Road")
-#
-# ShowTime.create!( begin_time: (Time.now), address: 209 Oberlin Rd, city: Raleigh, state: "NC", zip_code: 27605, date: Date.new)
-#
-# Performance.create!(name: "City Life", description: "Transactors Improv wraps up its main-stage season with City Life, a show about thriving and surviving in a major urban area. Featuring improvised songs, the two-act performance is based on audience suggestions and divided into five slice-of-life episodes.Over the years we Transactors have done lots of shows about small-town life but this is our first that focuses on the challenges and rewards of the big city. Strange and wonderful things happen to people when you cram a lot of them into a relatively small space.Of course we’ve done a lot of improvised music over the years and this show gives us a chance to continue the work we pioneered in this region. We’re very fortunate to have pianist Glenn Mehrbach as our accompanist.The company welcomes longtime member Steven Warnock (2001-14) back into the fold for City Life. He is joined in the cast by Anoo Brod, director Greg Hohn, Juliet Kaplan, Bart Hubbard, Steve Scott, and Jane Allen Wilson. Mike Beard is in charge of lights and sound.")
-#
-# ShowTime.create!( begin_time: (Time.now), address: 300-G E Main St, city: Carrboro, state: "NC", zip_code: 27510, date: Date.new)
-#
-# Performance.create!(name: "City Life", description: "Transactors Improv: For Families!")
-#
-# ShowTime.create!( begin_time: (Time.now), address: 300-G E Main St, city: Carrboro, state: "NC", zip_code: 27510, date: Date.new)
-#
-# Performance.create!(name: "Third Date Improv Spring Show", description: "Third Date returns to Common Ground Theatre for our Spring show. J, Nancy, Jenny and Dan will create an hour long improvised piece based on one suggestion. Third Date shows are a comic and sometimes bittersweet exploration of relationships with friends, family, partners and coworkers. They reflect the joys, surprises and everyday frustrations of life in our weird, wonderful world.")
-#
-# ShowTime.create!( begin_time: (Time.now), address: 4815B Hillsborough Rd, city: Durham, state: "NC", zip_code: 27705, date: Date.new)
