15a5151f9a741dbff6969be87de5e706555645f9
Steven Cooper <stevengcooper76@gmail.com>
2016-04-11 18:57:23 -0400

    User#index sends all of the users.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by(id: params[:id])
-    @companies = Company.where(user_id: params[:id]) if @user.is_professional 
+    @companies = Company.where(user_id: params[:id]) if @user.is_professional
   end
 
   def create
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
     else
       @success = false
     end
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
