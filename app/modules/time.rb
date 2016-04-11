30a08934842a89675f831372ccc4b5ec090d0ff7
Steven Cooper <stevengcooper76@gmail.com>
2016-04-11 13:24:17 -0400

    Deleted Time Module that wasn't used any more.

--- app/modules/time.rb
+++
@@ -1,6 +0,0 @@
-# module Time
-#   def upcoming_performance
-#     upcoming_show = @company.performances.select {|c| c.show_times.last.show_date > DateTime.now}
-#     upcoming_show.order(:show_date)
-#     upcoming_show[0]
-#   end
