af5d7902293c904a04f6aefa0a856e6a0ebcdef9
Steven Cooper <stevengcooper76@gmail.com>
2016-04-12 23:00:47 -0400

    Switching to another branch to bug-fix.

--- signature.rb
+++ signature.rb
@@ -2,18 +2,17 @@ require 'base64'
 require 'openssl'
 require 'digest/sha1'
 
-policy_document = {"expiration": "2019-01-01T00:00:00Z",
-  "conditions": [
-    {"bucket": "thescenery-attachments"},
-    ["starts-with", "image/jpeg", ""],
-    {"acl": "private"},
-    {"success_action_redirect": "http://localhost:3000"},
-    ["starts-with", "image/jpeg", ""],
-    ["content-length-range", 0, 1048576]
-  ]
-}
+policy_document = '{"expiration": "2019-01-01T00:00:00Z",
+ "conditions": [
+   {"bucket": "thescenery-attachments"},
+   ["starts-with", "$key", "uploads/"],
+   {"success_action_redirect": "https://the-scenery.com/#/userprofile"},
+   {"acl": "private"},
+   ["starts-with", "$Content-Type"]
+ ]
+}'
 
-policy = Base64.encode64(policy_document.to_s).gsub("\n","")
+policy = Base64.encode64(policy_document).gsub("\n","")
 
 signature = Base64.encode64(
     OpenSSL::HMAC.digest(
