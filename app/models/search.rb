b5b96bc83b77d1037e82b0437608571e4819baca
Brian Yarsawich <byarsawich@Brians-MacBook-Pro.local>
2016-04-14 12:37:10 -0400

    perfromance search refactored to use coalesce in search

--- app/models/search.rb
+++ app/models/search.rb
@@ -1,38 +1,25 @@
 class Search
   def self.performances(params)
-    if params[:city] == "" && params[:search_term] == "" && params[:state].downcase == "null" then
+    if params[:state] == "ZIP" then
       results = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
           where("genre_performances.genre_id = ? ", params[:genre_id]).
+          joins("LEFT JOIN show_times ON performances.id = show_times.event_id").
+          where("show_times.event_type = 'Performance' AND " +
+              "show_times.zip_code LIKE COALESCE(?, '%')  AND " +
+              "(performances.name LIKE COALESCE(?, '%') OR performances.description LIKE COALESCE(?, '%'))",
+                wildcard_param(params[:city]), wildcard_param(params[:search_term]), wildcard_param(params[:search_term])).
           distinct(:performance_id)
     else
-      if params[:state] == "ZIP" then
-        results = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
-            where("genre_performances.genre_id = ? ", params[:genre_id]).
-            joins("LEFT JOIN show_times ON performances.id = show_times.event_id").
-            where("show_times.event_type = 'Performance' AND " +
-                  "((? != '%%' AND show_times.zip_code LIKE ?)  OR " +
-                  "((LOWER(?) != '%%' AND LOWER(performances.name) LIKE LOWER(?)) OR (LOWER(?) != '%%' AND LOWER(performances.description) LIKE LOWER(?))))",
-                  "#{params[:city]}", "#{params[:city]}", "%#{params[:search_term]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%").
-            distinct(:performance_id)
-      else
-        results = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
-            where("genre_performances.genre_id = ? ", params[:genre_id]).
-            joins("LEFT JOIN show_times ON performances.id = show_times.event_id").
-            where("show_times.event_type = 'Performance' AND " +
-                  "((
-                    (LOWER(?) != '%%' AND LOWER(show_times.city) LIKE LOWER(?)) AND
-                    (LOWER(?) != '%%' AND LOWER(show_times.state) LIKE LOWER(?))
-                    )
-                  OR" +
-                  "(
-                  (LOWER(?) != '%%' AND LOWER(performances.name) LIKE LOWER(?)) OR
-                   (LOWER(?) != '%%' AND LOWER(performances.description) LIKE LOWER(?))
-                   ))" ,
-                  "%#{params[:city]}%",  "%#{params[:city]}%", "%#{params[:state]}%", "%#{params[:state]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%", "%#{params[:search_term]}%").
-            distinct(:performance_id)
-      end
+      results = Performance.joins("JOIN genre_performances on genre_performances.performance_id = performances.id").
+          where("genre_performances.genre_id = ? ", params[:genre_id]).
+          joins("LEFT JOIN show_times ON performances.id = show_times.event_id").
+          where("show_times.event_type = 'Performance' AND " +
+              "(show_times.city LIKE COALESCE(?, '%') AND show_times.state LIKE COALESCE(? , '%')) AND " +
+              "(performances.name LIKE COALESCE(?, '%') OR performances.description LIKE COALESCE(?, '%'))",
+                wildcard_param(params[:city]), wildcard_param(params[:state]), wildcard_param(params[:search_term]), wildcard_param(params[:search_term])).
+          distinct(:performance_id)
     end
-  end
+end
 
   def self.professionals(params)
     results = User.joins("LEFT JOIN user_titles ON user_titles.user_id = users.id").
@@ -52,4 +39,11 @@ class Search
     end
   end
 
+  def self.wildcard_param(value)
+    if value == nil or value == ""
+      nil
+    else
+      "%" + value + "%"
+    end
+  end
 end
