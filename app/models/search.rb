ccf7394eba32120260de041824f44ffeaa3c05d4
Steven Cooper <stevengcooper76@gmail.com>
2016-04-18 18:50:35 -0400

    Attempting to appease postgress with an order call.

--- app/models/search.rb
+++ app/models/search.rb
@@ -4,7 +4,6 @@ class Search
         where("genre_performances.genre_id = ? ", params[:genre_id]).
         joins("LEFT JOIN show_times ON performances.id = show_times.event_id").
         where("show_times.show_date >= ?", DateTime.now ).
-        order("show_times.show_date ASC").
         where("show_times.event_type = 'Performance' AND " +
             "(LOWER(performances.name) LIKE COALESCE(?, '%') OR LOWER(performances.description) LIKE COALESCE(?, '%'))",
             wildcard_param(params[:search_term]), wildcard_param(params[:search_term]))
@@ -15,6 +14,7 @@ class Search
       results = results.where("LOWER(show_times.city) LIKE COALESCE(?, '%') AND LOWER(show_times.state) LIKE COALESCE(? , '%')",wildcard_param(params[:city]),wildcard_param(params[:state]))
     end
 
+    results = results.order(show_date: :asc)
     results.distinct(:performance_id)
 end
 
